ddacb8d15c506f90431485db680a7521636fa34b
arsian <marshia370@gmail.com>
2025-03-04 23:24:21 +0330

    boss system

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -10,6 +10,11 @@ export default class Game extends Scene {
         this.bulSpeed = 500;
         this.canShot = true;
         this.enemy = null;
+        this.bosses = null;
+        this.bossDefeated = false;
+        this.bossesD = {
+            '1000': { name: 'Crazy Alien (Driver)', health: 1100, image: 'boss' }
+        }
         this.score = 0;
         this.scoreText = null;
         this.rejected = 0;
@@ -20,6 +25,7 @@ export default class Game extends Scene {
         this.centerY = null;
         this.hp = 3;
         this.hearts = [];
+        this.bossEffect = true;
     }
 
     preload() {
@@ -32,6 +38,9 @@ export default class Game extends Scene {
 
         // Enemy Ship Image
         this.load.image('enemyShip', '/enemyship.png');
+
+        // Bosses Images
+        this.load.image('boss', '/bosses/boss.png')
 
         // Bullets Image
         this.load.image('bullet', '/spacebullet.png');
@@ -85,14 +94,27 @@ export default class Game extends Scene {
         // Enemy Ship
         this.enemy = this.physics.add.group();
 
+        // Bosses
+        this.bosses = this.physics.add.group();
+        this.bossBulet = this.physics.add.group();
+        this.time.addEvent({
+            delay: 1000,
+            loop: true,
+            callback: () => {
+                this.bossShooting()
+            }
+        })
+
         // Potion
         this.posion = this.physics.add.group()
 
         // Spawning Enemy
         this.time.addEvent({
             delay: 1000, loop: true, callback: () => {
-                let enemy = this.physics.add.image(Math.Between(0, this.sys.game.config.width), 0, 'enemyShip').setScale(0.110);
-                this.enemy.add(enemy);
+                if (this.bossEffect) {
+                    let enemy = this.physics.add.image(Math.Between(0, this.sys.game.config.width), 0, 'enemyShip').setScale(0.110);
+                    this.enemy.add(enemy);
+                }
             }
         })
 
@@ -129,6 +151,9 @@ export default class Game extends Scene {
         // Enemy Logic
         this.enemyLogic()
 
+        // Bosse Spawnes
+        this.bossSpawn(1000 , '1000')
+
         // Collides
         this.physics.collide(this.bullets, this.enemy, (bullet, enemy) => {
 
@@ -142,16 +167,81 @@ export default class Game extends Scene {
             this.scoreText.setText('Score: ' + this.score);
         })
 
+        this.physics.collide(this.bullets, this.bosses, (bullet, boss) => {
+            bullet.destroy();
+
+            boss.health -= 20;
+
+            let healthPercent = boss.health / this.bossesD['1000'].health;
+
+            this.bossHealthBar.clear();
+            this.bossHealthBar.fillStyle(0x00ff00, 1);
+            this.bossHealthBar.fillRect(100, 570, window.Math.max(600 * healthPercent, 0), 20);
+
+            if (boss.health <= 0) {
+                this.bossEffect = true;
+                this.bossDefeated = true;
+                this.bossName.destroy();
+                boss.destroy();
+                this.bossHealthBar.clear();
+                this.bossHealthBarBG.clear();
+            }
+        });
+
+        this.physics.collide(this.ship, this.bosses, (ship, boss) => {
+
+            this.hp--;
+            let pushBackX = (ship.x - boss.x) * 2;
+            let pushBackY = (ship.y - boss.y) * 2;
+
+            ship.setVelocity(pushBackX, pushBackY);
+
+            this.time.delayedCall(500, () => {
+                if (ship.active) {
+                    ship.setVelocity(0);
+                }
+            })
+
+            if (this.hearts.length > 0) {
+                let lastHeart = this.hearts.pop();
+                lastHeart.destroy();
+            }
+
+            if (this.hp <= 0) {
+                this.playAnimation('expo', ship.x, ship.y, 'ani1');
+                ship.destroy();
+                this.add.text(180, 250, 'Game Over! \n press "SPACE" to continue', { fontSize: 30 });
+                this.isOver = true
+            }
+        })
+
+        this.physics.collide(this.ship, this.bossBulet, (ship, bullet) => {
+            bullet.destroy();
+            this.hp--;
+            if (this.hearts.length > 0) {
+                let lastHeart = this.hearts.pop();
+                lastHeart.destroy();
+            }
+
+            if (this.hp <= 0) {
+                this.playAnimation('expo', ship.x, ship.y, 'ani1');
+                ship.destroy();
+                this.add.text(180, 250, 'Game Over! \n press "SPACE" to continue', { fontSize: 30 });
+                this.isOver = true
+            }
+
+        })
+
         this.physics.collide(this.ship, this.enemy, (ship, enemy) => {
             this.damage(ship, enemy)
             this.playAnimation('expo', enemy.x, enemy.y, 'ani1');
         })
 
         // Potions
-        if (this.score > 0 && this.score % 100 == 0 && this.potionSpawned) {
+        if (this.score > 0 && this.score % 60 == 0 && this.potionSpawned) {
             this.posionSpawn();
             this.potionSpawned = false
-        } else if (this.score % 100 !== 0) {
+        } else if (this.score % 60 !== 0) {
             this.potionSpawned = true;
         }
 
@@ -198,16 +288,65 @@ export default class Game extends Scene {
         })
     }
 
+    bossSpawn(nededScore , selectBoss) {
+        if (this.score >= nededScore && !this.bossDefeated && this.bosses.getLength() === 0) {
+
+            this.bossEffect = false;
+            let selectedBoss = this.bossesD[selectBoss];
+
+            this.bossHealthBarBG = this.add.graphics();
+            this.bossHealthBarBG.fillStyle(0xff0000, 1);
+            this.bossHealthBarBG.fillRect(100, 570, 600, 20);
+
+            this.bossHealthBar = this.add.graphics();
+            this.bossHealthBar.fillStyle(0x00ff00, 1);
+            this.bossHealthBar.fillRect(100, 570, 600, 20);
+
+            let boss = this.physics.add.image(this.game.config.width / 2, 0, selectedBoss.image).setScale(0.6).setSize(755, 100);
+            boss.health = selectedBoss.health;
+            if (!this.bossDefeated) {
+                this.bossName = this.add.text((this.game.config.width / 2) - 300, 550, selectedBoss.name)
+            }
+
+            this.bosses.add(boss)
+            boss.setCollideWorldBounds(true);
+            boss.setBounce(1);
+            boss.setVelocityY(50);
+            boss.setImmovable(true);
+            boss.body.allowGravity = false;
+            this.time.delayedCall(3200, () => {
+                boss.setVelocityY(0)
+                boss.setVelocityX(35)
+            })
+        }
+    }
+
+    bossShooting() {
+        let boss = this.bosses.getChildren()[0];
+
+        if (boss) {
+            let bulletXPositions = [boss.x - 110, boss.x, boss.x + 110];
+
+            bulletXPositions.forEach(xPos => {
+                let bullet = this.physics.add.image(xPos, boss.y + 100, 'bullet').setSize(1, 1).setScale(0.0500);
+                this.bossBulet.add(bullet);
+                bullet.setVelocityY(500);
+            });
+        }
+    }
+
     enemyLogic() {
         this.enemy.getChildren().forEach(partOfEnemy => {
             if (partOfEnemy.active) {
                 partOfEnemy.setVelocityY(80);
                 if (partOfEnemy.y > this.game.config.height) {
                     partOfEnemy.destroy();
-                    this.score -= 20;
-                    this.rejected += 1;
-                    this.rejectText.setText('Rejecteds: ' + this.rejected);
-                    this.scoreText.setText('Score: ' + this.score);
+                    if (this.bossEffect) {
+                        this.score -= 20;
+                        this.rejected += 1;
+                        this.rejectText.setText('Rejecteds: ' + this.rejected);
+                        this.scoreText.setText('Score: ' + this.score);
+                    }
                 }
             }
         })
@@ -223,7 +362,7 @@ export default class Game extends Scene {
     }
 
     posionSpawn() {
-        let potionsTypes = ['speed', 'health' , 'bullSpeed'];
+        let potionsTypes = ['speed', 'health', 'bullSpeed'];
         let randomPotion = potionsTypes[Math.Between(0, potionsTypes.length - 1)]
 
         let posion = this.physics.add.image(Math.Between(0, this.game.config.width), 0, randomPotion).setScale(1.1);
@@ -267,7 +406,7 @@ export default class Game extends Scene {
                 break;
             case 'bullSpeed':
                 this.bulSpeed = 160;
-                this.time.delayedCall(5000 , () => this.bulSpeed = 500)
+                this.time.delayedCall(5000, () => this.bulSpeed = 500)
                 break;
         }
     }
